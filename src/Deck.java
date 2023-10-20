2bf73bdc8cc4adc20fe134e3ff4c4f44c862d413
mahnoor <mahnoorfatima404@gmail.com>
2023-10-20 13:09:47 -0400

    minor bug fixes to deck class, and adding functionality to main

--- src/Deck.java
+++ src/Deck.java
@@ -5,6 +5,7 @@ public class Deck {
 
     public Deck() {
         this.deck = new ArrayList<Card>();
+        createDeck();
     }
 
     public void createDeck() {
@@ -14,18 +15,19 @@ public class Deck {
             deck.add(new Card(Card.Rank.ZERO, Card.Color.values()[colors]));
 
             //adding two of ranks 1-9 and 3 special cards
-            for(int i =0; i<12; i++){
+            for(int i =1; i<13; i++){
                 for (int j =0; j<2; j++){
                     deck.add(new Card(Card.Rank.values()[i], Card.Color.values()[colors]));
                 }
             }
         }
         //adding four of Wild Cards
-        for(int i =12; i<14; i++){
+        for(int i =12; i<15; i++){
             for (int j =0; j<4; j++){
                 deck.add(new Card(Card.Rank.values()[i], Card.Color.WILD));
             }
         }
+        System.out.println(deck);
         this.shuffle();
     }
 
@@ -39,7 +41,8 @@ public class Deck {
     }
 
     public Card draw() {
-        return deck.remove(0);
+
+        return deck.remove(1);
     }
 
     public int size() {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("UNO FLIP GAME!");
+        UNOGame game = new UNOGame();
+        game.play();
     }
 }
