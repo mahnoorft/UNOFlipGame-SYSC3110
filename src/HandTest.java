1d4665b345f946eec1e0c78323bb6138a71bfde9
Areej504 <areej.abdelatif@gmail.com>
2023-10-21 14:01:22 -0400

    Created initial HandTest.java Class

---
+++ src/HandTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HandTest {
+
+    @org.junit.jupiter.api.Test
+    public void addCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void removeCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void removeAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void calculateTotalPoints() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getCards() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void isUNO() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void isEmpty() {
+    }
+}
