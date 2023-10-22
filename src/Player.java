110a589d36c8b1a013579fc929759650c329caf2
Areej504 <areej.abdelatif@gmail.com>
2023-10-22 13:11:51 -0400

    removed redundant addCard() method from Player Class

--- src/Player.java
+++ src/Player.java
@@ -21,9 +21,6 @@ public class  Player {
         Card drawnCard = deck.draw();
         hand.addCard(drawnCard);
     }
-    public void addCard(Card card){
-        hand.addCard(card);
-    }
     //update the player's score
     public void incrementScore(int addedScore) {
         this.score += addedScore;
