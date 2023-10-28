7904eddb6b6e075656699d198585e32fe3b13815
Areej504 <areej.abdelatif@gmail.com>
2023-10-28 13:38:56 -0400

    Created JFrame Class and Controller Class

---
+++ src/UNOGameController.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UNOGameController implements ActionListener {
+    UNOGame model;
+    UNOGameFrame view;
+    public UNOGameController(UNOGame model, UNOGameFrame view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
