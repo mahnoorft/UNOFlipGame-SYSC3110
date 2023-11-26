74709d942642f7dab34e2e259d85ad44d74c99ac
ramaalkhouli <108626403+ramaalkhouli@users.noreply.github.com>
2023-11-26 01:04:01 -0500

    changed size of all images to 130x182

--- src/Deck.java
+++ src/Deck.java
@@ -18,7 +18,7 @@ public class Deck {
         for (int colors = 0; colors < 4; colors++){
 
             //adding one rank 0
-            deck.add(new Card(Card.Rank.ZERO, Card.Color.values()[colors],Card.Rank.ZERO, Card.Color.values()[colors+4]));
+           // deck.add(new Card(Card.Rank.ZERO, Card.Color.values()[colors],Card.Rank.ZERO, Card.Color.values()[colors+4]));
 
             //adding two Cards of ranks 1-9
             for(int i =1; i<10; i++){
@@ -33,10 +33,11 @@ public class Deck {
                 deck.add(new Card(Card.Rank.FLIP, Card.Color.values()[colors],Card.Rank.FLIP, Card.Color.values()[colors+4]));
             }
         }
-        //adding four Cards of Wild and WildDraw2
+        //adding four Cards of Wild light  and wild dark , wild draw 2 and wild draw color
         for(int i =19; i<=20; i++){
             for (int j =0; j<4; j++){
                 deck.add(new Card(Card.Rank.WILD_LIGHT, Card.Color.WILD,Card.Rank.WILD_DARK, Card.Color.WILD));
+                deck.add(new Card(Card.Rank.DRAW2, Card.Color.WILD,Card.Rank.DRAW_COLOR, Card.Color.WILD));
             }
         }
         this.shuffle();
